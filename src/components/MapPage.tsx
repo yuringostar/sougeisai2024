111fedee65e5a4321390386d7d12f890bd4de1b9
yuri_takasu <takasuyt2653@gmail.com>
2024-08-28 20:41:13 +0900

    wip

--- src/components/MapPage.tsx
+++ src/components/MapPage.tsx
@@ -1,50 +1,40 @@
-import { motion } from 'framer-motion';
 import React, { useRef, useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
 import '../MapPage.css';
 import mapImage from '../assets/images/map_test.png';
+import { Stage, Layer, Rect, Image, Group } from 'react-konva';
 
 export const MapPage: React.FC = () => {
-    const containerRef = useRef<HTMLDivElement>(null);
+    /*const containerRef = useRef<HTMLDivElement>(null);
     const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [searchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();*/
+    let img = new window.Image();
+    img.src = mapImage;
+  
 
-    useEffect(() => {
+    /*useEffect(() => {
         const x = parseFloat(searchParams.get('x') || '0');
         const y = parseFloat(searchParams.get('y') || '0');
         if (!isNaN(x) && !isNaN(y)) {
             setPosition({ x, y });
         }
-    }, [searchParams]);
+    }, [searchParams]);*/
 
     return (
-        <div>
-            <motion.div ref={containerRef} className="container">
-                <motion.div 
-                    className="paper" 
-                    drag 
-                    dragPropagation 
-                    dragConstraints={containerRef}
-                    animate={position}
-                >
+        <>
+            
+            <div>
+                <Stage width={1200} height={1200} >
+                    <Layer>
+                        <Group>                      
+                            <Image image={img} width={img.width} height={img.height} x={300} /> 
+                        </Group>
+                    </Layer>                
+                </Stage>
+            </div>
+            
+        
+        
+        </>
 
-                    <div>
-                        <img src={mapImage} alt="map" />
-                    </div>
-                    {/*<div className="panel panel1" id='panel1'>
-                        <div className="text">Panel 1</div>
-                    </div>
-                    <div className="panel panel2" id='panel2'>
-                        <div className="text">Panel 2</div>
-                    </div>
-                    <div className="panel panel3">
-                        <div className="text">Panel 3</div>
-                    </div>
-                    <div className="panel panel4">
-                        <div className="text">Panel 4</div>
-                    </div>*/}
-                </motion.div>
-            </motion.div>
-        </div>
     );
 };
