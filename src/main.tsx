9b2a06071e0b874efeef2a390375c9e2727c6dc5
shimizuyasushi <51806272+kasuuuu4423@users.noreply.github.com>
2024-09-19 15:58:53 +0900

    ミス修正！！！！

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-
-// コンポーネントのインポート
-import { Layout } from './components/Layout';
-import { MapPage } from './components/MapPage';
-import { QrCodeScanner} from './components/QrCodeScanner';
+import App from './App';
+import './style.css';
 
 const container = document.getElementById('app');
-const root = createRoot(container!); 
+const root = createRoot(container!);
 
 root.render(
-    <React.StrictMode>
-
-    <BrowserRouter>
-        
-        <Routes>
-            
-            <Route path="/" element={<Layout />}>
-                <Route path="/map" element={<QrCodeScanner />} />
-            </Route>
-        </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
-
+    // <React.StrictMode>
+        <App />
+    // </React.StrictMode>
 );
